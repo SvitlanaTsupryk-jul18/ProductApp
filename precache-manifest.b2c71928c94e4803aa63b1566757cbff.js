944488d34f8807dd02ebfe70ad4cbe991393baba
Svet <stsupryk@gmail.com>
2019-08-17 06:02:49 +0300

    Updates

---
+++ precache-manifest.b2c71928c94e4803aa63b1566757cbff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100f3cd2f579a25c441b2ef81f3fee66",
+    "url": "/ProductApp/index.html"
+  },
+  {
+    "revision": "9e960678a78e7fef4d2d",
+    "url": "/ProductApp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "59bd54e16a1b5b19f92a",
+    "url": "/ProductApp/static/js/2.95c9d5e8.chunk.js"
+  },
+  {
+    "revision": "9e960678a78e7fef4d2d",
+    "url": "/ProductApp/static/js/main.a480f732.chunk.js"
+  },
+  {
+    "revision": "71024b4d4e2f25d78da8",
+    "url": "/ProductApp/static/js/runtime~main.0ab52380.js"
+  }
+]);
